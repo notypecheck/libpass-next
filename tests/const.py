44697da05341928afb2ef8705ea0820e1f23aa9a
Doctor <thirvondukr@gmail.com>
2025-02-22 08:59:19 +0300

    move libpass stuff from passlib repository

---
+++ tests/const.py
@@ -0,0 +1,4 @@
+UPASS_WAV = "\u0399\u03c9\u03b1\u03bd\u03bd\u03b7\u03c2"
+UPASS_USD = "\u20ac\u00a5$"
+UPASS_TABLE = "t\u00e1\u0411\u2113\u0259"
+PASS_TABLE_UTF8 = b"t\xc3\xa1\xd0\x91\xe2\x84\x93\xc9\x99"  # utf-8
